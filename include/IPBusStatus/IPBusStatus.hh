142a742da6758d74c34e5e5c8663fba76a14448d
Amitav Mitra <amitav.mitra@cern.ch>
2021-05-20 16:26:14 -0400

    fix missing boost::unordered_map call

--- include/IPBusStatus/IPBusStatus.hh
+++ include/IPBusStatus/IPBusStatus.hh
@@ -3,6 +3,15 @@
 
 #include <IPBusIO/IPBusIO.hh>
 #include <BUTool/helpers/StatusDisplay/StatusDisplay.hh>
+
+#ifdef STD_UNORDERED_MAP
+#include <unordered_map>
+typedef std::unordered_map<std::string, std::string> uMap;
+#else 
+#include <boost/unordered_map.hpp>
+typedef boost::unordered_map<std::string, std::string> uMap;
+#endif
+
 class IPBusStatus: public IPBusIO, public BUTool::StatusDisplay {
 public:
   IPBusStatus(uhal::HwInterface * const * _hw){SetHWInterface(_hw);};
