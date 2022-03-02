ae13c6686956d64ee1ff3b9ca829f4e1ac669f85
cms <aakpinar@bu.edu>
2022-03-02 22:36:18 +0000

    Add the ReadConvert methods to IPBusIO class

--- include/IPBusIO/IPBusIO.hh
+++ include/IPBusIO/IPBusIO.hh
@@ -40,11 +40,30 @@ public:
   //Node reads
   uint32_t RegReadNode(uhal::Node const & node);
 
+  // Conversion format
+  std::string RegReadConvertFormat(std::string const & reg);
+ 
+  // Named register read+conversion functions, overloaded depending on the conversion value type
+  void RegReadConvert(std::string const & reg, unsigned int & val);
+  void RegReadConvert(std::string const & reg, int & val);
+  void RegReadConvert(std::string const & reg, double & val); 
+  void RegReadConvert(std::string const & reg, std::string & val);
+
   //numeric, named register, action, and node writes
   void RegWriteAddress(uint32_t addr, uint32_t data);
   void RegWriteRegister(std::string const & reg, uint32_t data);
   void RegWriteAction(std::string const & reg);
   void RegWriteNode(uhal::Node const & node, uint32_t data);
+
+  // Helper functions for converting
+  double ConvertFloatingPoint16ToDouble(std::string const & reg);
+  double ConvertLinear11ToDouble(std::string const & reg);
+  double ConvertIntegerToDouble(std::string const & reg, std::string const & format);
+  std::string ConvertEnumToString(std::string const & reg, std::string const & format);
+  std::string ConvertIPAddressToString(std::string const & reg);
+
+  // Helper function to return list of register names with a specified parameter
+  std::vector<std::string> GetRegisterNamesFromTable(std::string const & tableName);
 
   uhal::Node const & GetNode(std::string const & reg);
 protected:
--- src/IPBusIO/IPBusIO.cpp
+++ src/IPBusIO/IPBusIO.cpp
@@ -3,6 +3,18 @@
 #include <IPBusIO/IPBusIO.hh>
 #include <IPBusIO/IPBusExceptions.hh>
 
+#include <stdlib.h> //strtoul
+#include <map> //map
+#include <math.h> // for NAN
+#include <cmath> // for pow
+#include <arpa/inet.h> // for inet_ntoa and in_addr_t
+
+#include <boost/algorithm/string/case_conv.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+
+#define DISPLAY_DEFAULT_FORMAT "X"
+
+using boost::algorithm::iequals;
 
 inline void CheckHW(uhal::HwInterface * const * hw){
   if(NULL == hw){
@@ -97,6 +109,287 @@ uint32_t IPBusIO::RegReadNode(uhal::Node const & node){
   }
 
   return vw.value();
+}
+
+double IPBusIO::ConvertFloatingPoint16ToDouble(std::string const & reg){
+  // Helper function to do the "fp16->double" conversion
+  double doubleVal;
+
+  union {
+    struct {
+      uint16_t significand   : 10;
+      uint16_t exponent      : 5;
+      uint16_t sign          : 1;
+    } fp16;
+    int16_t raw;
+  } val;
+  val.raw = RegReadRegister(reg);
+
+  switch (val.fp16.exponent) {
+  // Case where the exponent is minimum
+  case 0:
+    if (val.fp16.significand == 0) {
+      doubleVal = 0.0;
+    }
+    else {
+      doubleVal = pow(2,-14)*(val.fp16.significand/1024.0);
+    }
+
+    // Apply sign
+    if (val.fp16.sign) {
+      doubleVal *= -1.0;
+    }
+    break;
+  // Case where the exponent is maximum
+  case 31:
+    if (val.fp16.significand == 0) {
+      doubleVal = INFINITY;
+      if (val.fp16.sign) {
+	doubleVal *= -1.0;
+      }   
+    }
+    else {
+      doubleVal = NAN;
+    }
+    break;
+  // Cases in between
+  default:
+    doubleVal = pow(2, val.fp16.exponent-15)*(1.0+(val.fp16.significand/1024.0));
+    if (val.fp16.sign) {
+      doubleVal *= -1.0;
+    }
+    break;   
+  }
+  return doubleVal;
+}
+
+double IPBusIO::ConvertIntegerToDouble(std::string const & reg, std::string const & format){
+  // Helper function to convert an integer to float using the following format:
+  // y = (sign)*(M_n/M_d)*x + (sign)*(b_n/b_d)
+  //       [0]   [1] [2]        [3]   [4] [5]
+  // sign == 0 -> negative, other values mean positive
+
+  std::vector<uint64_t> mathValues;
+  size_t iFormat=1;
+ 
+  uint32_t rawVal = RegReadRegister(reg);
+ 
+  while (mathValues.size() != 6 && iFormat < format.size()) {
+    if (format[iFormat] == '_') {
+      // Start parsing the value after the '_' and add the corresponding value to mathValues array
+      for (size_t jFormat=++iFormat; jFormat < format.size(); jFormat++) {
+        if ( (format[jFormat] == '_') || (jFormat == format.size() - 1) ) {
+	  if (jFormat == format.size() - 1) { jFormat++; }
+          // Convert the string to a number
+          uint64_t val = strtoull(format.substr(iFormat, jFormat-iFormat).c_str(), NULL, 0);
+          mathValues.push_back(val);
+          iFormat = jFormat;
+          break;
+        }
+      }
+    }
+    else {
+      iFormat++;
+    } 
+  }
+
+  // TODO: Some checks needed, check that mathValues.size() == 6, and no 0s in denominator
+
+  // Compute the transformed value from the raw value
+  // Will compute: (m*x) + b
+  double transformedValue = rawVal;
+  transformedValue *= double(mathValues[1]);
+  transformedValue /= double(mathValues[2]);
+  // Apply the sign of m
+  if (mathValues[0] == 0) {
+    transformedValue *= -1;
+  }
+  
+  double b = double(mathValues[4]) / double(mathValues[5]);
+  if (mathValues[3] == 0) {
+    b *= -1;
+  }
+  transformedValue += b;
+
+  return transformedValue;
+}
+
+double IPBusIO::ConvertLinear11ToDouble(std::string const & reg){
+  // Helper function to convert linear11 format to double
+
+  union {
+    struct {
+      int16_t integer  : 11;
+      int16_t exponent :  5;
+    } linear11;
+    int16_t raw;
+  } val;
+  
+  val.raw = RegReadRegister(reg);
+  double floatingValue = double(val.linear11.integer) * pow(2, val.linear11.exponent);
+  
+  return floatingValue;
+}
+
+std::string IPBusIO::ConvertIPAddressToString(std::string const & reg){
+  // Helper function to convert IP addresses to string
+  struct in_addr addr;
+  int16_t val = RegReadRegister(reg);
+  addr.s_addr = in_addr_t(val);
+
+  return inet_ntoa(addr);
+}
+
+std::string IPBusIO::ConvertEnumToString(std::string const & reg, std::string const & format){
+  // Helper function to convert enum to std::string
+  std::map<uint64_t, std::string> enumMap;
+  
+  // Parse the format argument for enumerations
+  size_t iFormat = 1;
+  while (iFormat < format.size()) {
+    if (format[iFormat] == '_') {
+      // Read the integer corresponding to this enum
+      uint64_t val = 0;
+      for (size_t jFormat = ++iFormat; jFormat < format.size(); jFormat++) {
+        if ((format[jFormat] == '_') | (jFormat == format.size() - 1)) {
+          if (jFormat == format.size() - 1) { jFormat++; }
+          // Convert the value into number
+          val = strtoul(format.substr(iFormat, jFormat-iFormat).c_str(), NULL, 0);
+          iFormat = jFormat;
+          break;
+        }
+      }
+      // Now read the corresponding string
+      for (size_t jFormat = ++iFormat; jFormat < format.size(); jFormat++) {
+        if ((format[jFormat] == '_') || (jFormat == (format.size()-1))) {
+          if (jFormat == format.size() - 1) { jFormat++; }
+          // Get the string
+          enumMap[val] = format.substr(iFormat, jFormat-iFormat);
+          iFormat = jFormat;
+          break;  
+        }
+      } 
+    }
+    else {
+      iFormat++;
+    }
+  }
+
+  // Now we have the enumeration map, read the integer value from the register
+  // Then return the corresponding string
+  uint32_t regValue = RegReadRegister(reg);
+  if (enumMap.find(regValue) != enumMap.end()) {
+    // If format starts with 't', just return the string value
+    // Otherwise, return the value together with the number
+    if (format[0] == 't') {
+      return enumMap[regValue].c_str();
+    }
+    return (enumMap[regValue] + " " + std::to_string(regValue)).c_str();
+  }
+
+  // Cannot find it, TODO: better handle this possibility
+  return "NOT_FOUND";
+}
+
+std::vector<std::string> IPBusIO::GetRegisterNamesFromTable(std::string const & tableName){ 
+  // Helper function to get a list of register names from a given table name 
+  std::vector<std::string> registerNames;
+
+  // All register names
+  std::vector<std::string> allNames = myMatchRegex("*");
+
+  for (size_t idx=0; idx < allNames.size(); idx++) {
+    const uMap parameters = GetParameters(allNames[idx]);
+    const std::string table = (parameters.find("Table") != parameters.end()) ? parameters.find("Table")->second : "Not found";
+    if (table == tableName) {
+      registerNames.push_back(allNames[idx]);
+    }
+  }
+
+  return registerNames;
+}
+
+std::string IPBusIO::RegReadConvertFormat(std::string const & reg){
+  // From a given node address, retrieve the "Format" parameter of the node
+  const uMap parameters = GetParameters(reg);
+  std::string format = (parameters.find("Format") != parameters.end()) ? parameters.find("Format")->second : DISPLAY_DEFAULT_FORMAT;
+  return format; 
+}
+
+void IPBusIO::RegReadConvert(std::string const & reg, unsigned int & val){
+  // Read the value from the named register, and update the value in place
+  uint32_t rawVal = RegReadRegister(reg);
+  val = rawVal;
+}
+
+void IPBusIO::RegReadConvert(std::string const & reg, int & val){
+  // Read the value from the named register, and update the value in place
+  uint32_t rawVal = RegReadRegister(reg);
+
+  // Now comes the check:
+  // -55 is a placeholder for non-running fireflies
+  // That would mean rawVal 256 + (-55) = 201 since the raw value is an unsigned int
+  // We'll transform and return that value, in other cases our job is easier
+  int MAX_8_BIT_INT = 256;
+  if (rawVal == 201) {
+    val = -(int)(MAX_8_BIT_INT - rawVal);
+    return;
+  }
+  val = (int)rawVal;
+ 
+}
+
+void IPBusIO::RegReadConvert(std::string const & reg, double & val){
+  // Read the value from the named register, and update the value in place
+  // Check the conversion type we want:
+  // Is it a "fp16", or will we do some transformations? (i.e."m_...")
+  std::string format = RegReadConvertFormat(reg);
+  // 16-bit floating point to double transformation
+  if (iequals(format, "fp16")) {
+    val = ConvertFloatingPoint16ToDouble(reg);
+  }
+  
+  // Need to do some arithmetic to transform
+  else if ((format[0] == 'M') | (format[0] == 'm')) {
+    val = ConvertIntegerToDouble(reg, format);
+  }
+
+  else if (iequals(format, "linear11")) {
+    val = ConvertLinear11ToDouble(reg);
+  }
+
+  // Undefined format, throw error
+  else {
+    BUException::FORMATTING_NOT_IMPLEMENTED e;
+    e.Append("Format: " + format);
+    e.Append("\n");
+    throw e;
+  }
+}
+
+void IPBusIO::RegReadConvert(std::string const & reg, std::string & val){
+  // Read the value from the named register, and update the value in place
+  std::string format = RegReadConvertFormat(reg);
+  
+  if ((format.size() > 1) && (('t' == format[0]) || ('T' == format[0]))) {
+    val = ConvertEnumToString(reg, format);
+  }
+  // IP addresses
+  else if (iequals(format, std::string("IP"))) {
+    val = ConvertIPAddressToString(reg);
+  }
+  // Hex numbers in string
+  else if (iequals(format, "x")) {
+    val = RegReadRegister(reg);
+  }
+  // Undefined format, throw error
+  else {
+    BUException::FORMATTING_NOT_IMPLEMENTED e;
+    e.Append("Format: " + format);
+    e.Append("\n");
+    throw e;
+  }
+
 }
 
 void IPBusIO::RegWriteAction(std::string const & reg){
