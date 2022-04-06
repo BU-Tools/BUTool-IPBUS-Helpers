d5c64dc3f667bc8e989d5f5a4682fbd38eb14b9b
Dan Gastler <dgastler@gmail.com>
2022-04-06 17:46:37 +0000

    update to code and libs

--- src/IPBusRegHelper/IPBusRegHelper.cpp
+++ src/IPBusRegHelper/IPBusRegHelper.cpp
@@ -1,12 +1,12 @@
-#include <IPBusRegHelper.hh>
+#include <IPBusRegHelper/IPBusRegHelper.hh>
 
 IPBusRegHelper::IPBusRegHelper(std::shared_ptr<IPBusIO> _regIO,
 			       std::shared_ptr<BUTextIO> _textIO) :
   //Recast the IPBusIO to its base class 
-  RegisterHelper(std::shared_ptr<RegIO>(regIO),_textIO) {
+  RegisterHelper(std::shared_ptr<BUTool::RegisterHelperIO>(_regIO),_textIO) {
 }
 
-std::shared_ptr<IPBusIO> & IPBusRegHelper::GetRegIO(){
+std::shared_ptr<IPBusIO> IPBusRegHelper::GetRegIO(){
   //return a reference since this in internal to the class and is just a re-casting
   return std::dynamic_pointer_cast<IPBusIO>(regIO);
 }
