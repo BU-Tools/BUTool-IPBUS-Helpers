fb7534aa87f8ee93f623a368f19419f50e7cd9c4
Dan Gastler <dgastler@gmail.com>
2022-04-11 12:23:31 -0400

    Fixed incorrect casting of passed textio objects

--- src/IPBusRegHelper/IPBusRegHelper.cpp
+++ src/IPBusRegHelper/IPBusRegHelper.cpp
@@ -3,7 +3,8 @@
 IPBusRegHelper::IPBusRegHelper(std::shared_ptr<IPBusIO> _regIO,
 			       std::shared_ptr<BUTextIO> _textIO) :
   //Recast the IPBusIO to its base class 
-  RegisterHelper(std::shared_ptr<BUTool::RegisterHelperIO>(_regIO),_textIO) {
+  RegisterHelper(std::static_pointer_cast<BUTool::RegisterHelperIO>(_regIO),
+		 _textIO) {
 }
 
 std::shared_ptr<IPBusIO> IPBusRegHelper::GetRegIO(){
