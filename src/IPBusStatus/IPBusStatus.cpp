aee1307c543857a1a40db69c1f0f9413a0e60846
Alp Akpinar <aakpinar@bu.edu>
2022-05-11 19:31:05 -0400

    Pass RegIO pointer to StatusDisplayMatrix

--- src/IPBusStatus/IPBusStatus.cpp
+++ src/IPBusStatus/IPBusStatus.cpp
@@ -9,46 +9,33 @@ IPBusStatus::IPBusStatus(IPBusIO * _regIO):
 };
 
 void IPBusStatus::Process(std::string const & singleTable){  
-  //Build tables
+  // Build tables
   std::vector<std::string> Names = regIO->GetRegsRegex("*");
-  //process all the nodes and build table structure
+  // Process all the nodes and build table structure
   for(std::vector<std::string>::iterator itName = Names.begin();
       itName != Names.end();
       itName++){
-    //Get the list of parameters for this node (this is only in IPBusIO
+    // Get the list of parameters for this node (this is only in IPBusIO)
     uMap parameters = dynamic_cast<IPBusIO*>(regIO)->GetParameters(*itName);
 
     //In IPBus the description is separate, so we add it to parameters here
     parameters["Description"] = regIO->GetRegDescription(*itName);
 
-    if(parameters.size() != 0){      
-      for(uMap::iterator itTable = parameters.begin();
-	  itTable != parameters.end();
-	  itTable++){
-      }
-    }
     //Look for a Status parameter
     if(parameters.find("Status") != parameters.end()){        
-      //Check for an table name
+      // Check for a table name
       uMap::iterator itTable = parameters.find("Table");
       
       std::string tableName = itTable->second;
       //Add this Address to our Tables if it matches our singleTable option, or we are looking at all tables
       if( singleTable.empty() || TableNameCompare(tableName,singleTable)){
-	uint32_t val;
-	try{
-	  val = regIO->ReadRegister(*itName);
-#ifdef USE_UIO_UHAL
-	}catch(uhal::exception::UIOBusError & e){
-	  continue;
-#endif
-	}catch(std::exception & e){
-	  throw e;
-	}
-	tables[tableName].Add(*itName,
-			      val,
-			      regIO->GetRegMask(*itName),
-			      parameters);	
+        // Add the register to the given table, with a pointer to a RegHelperIO
+        // class to read values later.
+        tables[tablename].Add(*itname, regIO);
+        // tables[tableName].Add(*itName,
+        // 		      val,
+        // 		      regIO->GetRegMask(*itName),
+        // 		      parameters);	
       }
     }
   }
